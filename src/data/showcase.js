5872518525cfdb88d15ef696a0692ea45b4583dd
Dawn-o <ruselaq@gmail.com>
2026-01-17 11:29:03 +0800

    fix: Add missing trailing comma in showcase data

--- src/data/showcase.js
+++ src/data/showcase.js
@@ -526,7 +526,7 @@ export default [
         summary: "Founded by anime enthusiasts from the Middle East, Anime-Dunya.com is a free, ad-free streaming community designed to bridge cultures by providing premium Full HD anime with tailored multilingual support.",
         imageUrl: "https://cdn.anime-dunya.com/misc/screenshot.png",
         url: "https://anime-dunya.com",
-    }
+    },
     {
       title: "Morime",
       summary:
